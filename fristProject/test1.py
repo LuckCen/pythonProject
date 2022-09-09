8448407bb19381757bf15bcab2c97762d0b219c5
LuckCen <1070857438@qq.com>
2022-09-09 11:21:41 +0800

    init project

---
+++ fristProject/test1.py
@@ -0,0 +1,8 @@
+def test1():
+    num = [0,0,0]
+    c = set(num)
+    print(c)
+    for q in c:
+        if q == 0:
+            print([0, 0, 0])
+test1()
---
+++ mysite/blog/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+# Create your tests here.
+def test1():
+    # list1 = [[5,8],[6,3],[3,1]]
+    # list1.sort(key=lambda k: (k[1],k[0]))
+    # print(list1)
+
+    dic = {}
+    dic['A'] = 1
+    ls = ['a',1]
+    print(''.join(ls))
+    print( dic)
+
+
+
